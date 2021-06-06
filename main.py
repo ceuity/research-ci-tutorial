f0a96e43175afad735d285856caa81d38080aacc
ceuity <everland7942@gmail.com>
2021-06-06 21:28:12 +0900

    Update: dup

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ def helloworld(s: str):
     return msg
 
 
+def helloworld(s: str):
+    msg = f"Hello World! {s}!"
+    print(msg)  # hmm
+    return msg
+
+
 if __name__ == "__main__":  # pragma: no cover
     helloworld("Hyunseung Lee")
